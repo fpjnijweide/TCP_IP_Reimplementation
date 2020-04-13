24c5a88d617a72b1f13df14459aaa24db3bba31c
Freek Nijweide <freeknijweide@gmail.com>
2020-04-13 11:54:12 +0200

    added state, bigpacket and smallpacket class

--- MyProtocol.java
+++ MyProtocol.java
@@ -11,14 +11,52 @@ import java.util.concurrent.LinkedBlockingQueue;
 * Feel free to modify this code in any way you like!
 */
 
-public class MyProtocol{
 
+public class MyProtocol{
+    public enum State{
+        NEGOTIATING,
+        READY
+    }
+
+    public class SmallPacket{
+        int sourceIP;
+        int destIP;
+        int ackNum;
+        boolean ackFlag;
+        boolean morePackFlag;
+        boolean request;
+        boolean negotiate;
+        boolean SYN;
+
+        public SmallPacket(int sourceIP, int destIP, int ackNum, boolean ackFlag, boolean morePackFlag, boolean request, boolean negotiate, boolean SYN) {
+            this.sourceIP = sourceIP;
+            this.destIP = destIP;
+            this.ackNum = ackNum;
+            this.ackFlag = ackFlag;
+            this.morePackFlag = morePackFlag;
+            this.request = request;
+            this.negotiate = negotiate;
+            this.SYN = SYN;
+        }
+    }
+
+    public class BigPacket extends SmallPacket{
+        byte[] toSend;
+        int seqNum;
+
+        public BigPacket(int sourceIP, int destIP, int seqNum, int ackNum, boolean ackFlag, boolean morePackFlag, byte[] toSend, boolean request, boolean negotiate, boolean SYN) {
+            super(sourceIP, destIP, ackNum, ackFlag, morePackFlag, request, negotiate, SYN);
+            this.seqNum = seqNum;
+            this.toSend = toSend;
+        }
+    }
     // The host to connect to. Set this to localhost when using the audio interface tool.
     private static String SERVER_IP = "netsys2.ewi.utwente.nl"; //"127.0.0.1";
     // The port to connect to. 8954 for the simulation server.
     private static int SERVER_PORT = 8954;
     // The frequency to use.
     private static int frequency = 5400;
+    private State state = State.NEGOTIATING;
 
     private BlockingQueue<Message> receivedQueue;
     private BlockingQueue<Message> sendingQueue;
@@ -31,6 +69,12 @@ public class MyProtocol{
 
         new receiveThread(receivedQueue).start(); // Start thread to handle received messages!
 
+
+
+
+
+
+
         // handle sending from stdin from this thread.
         try{
             ByteBuffer temp = ByteBuffer.allocate(1024);
@@ -39,7 +83,10 @@ public class MyProtocol{
                 read = System.in.read(temp.array()); // Get data from stdin, hit enter to send!
                 System.out.println(read-1);
 
-                if(read > 0){
+                if (state==State.NEGOTIATING) {
+                    System.out.println("Can't send messages while negotiating");
+                } else if(read > 0){
+                    // TODO when no longer negotiating: send out something in the request phase, and in the data phase
                     ByteBuffer toSend;
                     if(read >2) {
                         toSend = ByteBuffer.allocate(read-1); // jave includes newlines in System.in.read, so -2 to ignore this
@@ -67,11 +114,15 @@ public class MyProtocol{
             System.exit(2);
         }        
     }
-    public int[] fillSmallPacket(int sourceIP, int destIP, int seqNum, int ackNum, boolean ackFlag, boolean morePackFlag, byte[] toSend, boolean request, boolean negotiate, boolean SYN ) {
-        int first_byte = sourceIP << 6 | destIP << 4 | (ackFlag ? 1:0) << 3 | (request ? 1:0) << 2 | (negotiate ? 1:0) << 1 | (SYN ? 1:0);
-        int second_byte = (morePackFlag ? 1:0) << 7 | seqNum << 5 | ackNum << 3;
+
+
+    public int[] fillBigPacket(BigPacket packet) {
+        // TODO refactor into smallpacket
+        int first_byte = packet.sourceIP << 6 | packet.destIP << 4 | (packet.ackFlag ? 1:0) << 3 | (packet.request ? 1:0) << 2 | (packet.negotiate ? 1:0) << 1 | (packet.SYN ? 1:0);
+        int second_byte = (packet.morePackFlag ? 1:0) << 7 | packet.seqNum << 5 | packet.ackNum << 3;
         return new int[]{first_byte, second_byte};
     }
+
 
     public static void main(String args[]) {
         if(args.length > 0){
@@ -101,6 +152,8 @@ public class MyProtocol{
 
         }
 
+        // TODO discovery phase: network toplogy
+
         public void run(){
             while(true) {
                 try{
@@ -112,9 +165,11 @@ public class MyProtocol{
                     } else if (m.getType() == MessageType.DATA){
                         System.out.print("DATA: ");
                         printByteBuffer( m.getData(), m.getData().capacity() ); //Just print the data
+
                     } else if (m.getType() == MessageType.DATA_SHORT){
                         System.out.print("DATA_SHORT: ");
                         printByteBuffer( m.getData(), m.getData().capacity() ); //Just print the data
+                        // TODO read data, check if negotiating
                     } else if (m.getType() == MessageType.DONE_SENDING){
                         System.out.println("DONE_SENDING");
                     } else if (m.getType() == MessageType.HELLO){
